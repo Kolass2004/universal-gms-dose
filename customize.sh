6debdeeb66bfcfdee48edc21dfbe458bfdfb15df
Kolass rexon J <89210243+Kolass2004@users.noreply.github.com>
2024-06-14 20:13:49 +0530

    initial

---
+++ customize.sh
@@ -0,0 +1,113 @@
+#!/data/adb/magisk/busybox sh
+set -o standalone
+
+set -x
+
+
+# Check root environment
+VER=`grep_prop version $MODPATH/module.prop`
+VERCODE=`grep_prop versionCode $MODPATH/module.prop`
+ui_print "  ID: $MODID"
+ui_print "  Version: $VER"
+ui_print "  VersionCode: $VERCODE"
+if [ "$KSU" == true ]; then
+ui_print "  KSUVersion: $KSU_VER"
+ui_print "  KSUVersionCode: $KSU_VER_CODE"
+ui_print "  KSUKernelVersionCode: $KSU_KERNEL_VER_CODE"
+else
+ui_print "  MagiskVersion: $MAGISK_VER"
+ui_print "  MagiskVersionCode: $MAGISK_VER_CODE"
+fi
+ui_print " "
+
+# Check Android API
+[ $API -ge 23 ] ||
+ abort "- Unsupported API version: $API"
+
+# Patch the XML and place the modified one to the original directory
+ui_print "- Patching XML files"
+{
+GMS0="\"com.google.android.gms"\"
+STR1="allow-in-power-save package=$GMS0"
+STR2="allow-in-data-usage-save package=$GMS0"
+NULL="/dev/null"
+}
+ui_print "- Searching default XML files"
+SYS_XML="$(
+SXML="$(find /system_ext/* /system/* /product/* \
+/vendor/* -type f -iname '*.xml' -print)"
+for S in $SXML; do
+if grep -qE "$STR1|$STR2" $ROOT$S 2> $NULL; then
+echo "$S"
+fi
+done
+)"
+
+PATCH_SX() {
+for SX in $SYS_XML; do
+mkdir -p "$(dirname $MODPATH$SX)"
+cp -af $ROOT$SX $MODPATH$SX
+ ui_print "  Patching: $SX"
+sed -i "/$STR1/d;/$STR2/d" $MODPATH/$SX
+done
+
+# Merge patched files under /system dir
+for P in product vendor; do
+if [ -d $MODPATH/$P ]; then
+ ui_print "- Moving files to module directory"
+mkdir -p $MODPATH/system/$P
+mv -f $MODPATH/$P $MODPATH/system/
+fi
+done
+}
+
+# Search and patch any conflicting modules (if present)
+# Search conflicting XML files
+MOD_XML="$(
+MXML="$(find /data/adb/* -type f -iname "*.xml" -print)"
+for M in $MXML; do
+if grep -qE "$STR1|$STR2" $M; then
+echo "$M"
+fi
+done
+)"
+
+PATCH_MX() {
+ ui_print "- Searching conflicting XML"
+for MX in $MOD_XML; do
+MOD="$(echo "$MX" | awk -F'/' '{print $5}')"
+ ui_print "  $MOD: $MX"
+sed -i "/$STR1/d;/$STR2/d" $MX
+done
+}
+
+# Find and patch conflicting XML
+PATCH_SX && PATCH_MX
+
+# Additional add-on for check gms status
+ADDON() {
+ ui_print "- Inflating add-on file"
+mkdir -p $MODPATH/system/bin
+mv -f $MODPATH/gmsc $MODPATH/system/bin/gmsc
+}
+
+FINALIZE() {
+ ui_print "- Finalizing installation"
+
+# Clean up
+ ui_print "  Cleaning obsolete files"
+find $MODPATH/* -maxdepth 0 \
+! -name 'module.prop' \
+! -name 'post-fs-data.sh' \
+! -name 'service.sh' \
+! -name 'system' \
+-exec rm -rf {} \;
+
+# Settings dir and file permission
+ ui_print "  Settings permissions"
+set_perm_recursive $MODPATH 0 0 0755 0755
+set_perm $MODPATH/system/bin/gmsc 0 2000 0755
+}
+
+# Final adjustment
+ADDON && FINALIZE
---
+++ service.sh
@@ -0,0 +1,34 @@
+#!/data/adb/magisk/busybox sh
+set -o standalone
+
+#
+# Universal GMS Doze by the
+# open-source loving GL-DP and all contributors;
+# Patches Google Play services app and certain processes/services to be able to use battery optimization
+#
+
+(   
+# Wait until boot completed
+until [ $(resetprop sys.boot_completed) -eq 1 ] &&
+[ -d /sdcard ]; do
+sleep 100
+done
+
+# GMS components
+GMS="com.google.android.gms"
+GC1="auth.managed.admin.DeviceAdminReceiver"
+GC2="mdm.receivers.MdmDeviceAdminReceiver"
+NLL="/dev/null"
+
+# Disable collective device administrators
+for U in $(ls /data/user); do
+for C in $GC1 $GC2 $GC3; do
+pm disable --user $U "$GMS/$GMS.$C" &> $NLL
+done
+done
+
+# Add GMS to battery optimization
+dumpsys deviceidle whitelist -com.google.android.gms &> $NLL
+
+exit 0
+)
